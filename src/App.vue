c7937035998fe1a0fa9aa9038dafb574264562b0
k-kudo-hub <tongariisaibou@gmail.com>
2020-11-15 11:30:40 +0900

    CSSアニメーションを使ってslideするトランジション効果を作成する

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
     <transition name="fade">
       <p v-if="show">hello</p>
     </transition>
+    <transition name="slide">  
+      <p v-if="show">bye</p>
+    </transition>
   </div>
 </template>
 
@@ -41,6 +44,23 @@ export default {
 .fade-leave-to {
   /* 消えるときの最後の状態 */
   opacity: 0;
+}
+
+
+.slide-enter-active {
+  animation: slide-in 0.5s;
+}
+.slide-leave-active {
+  animation: slide-in 0.5s reverse;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateX(100px)
+  }
+  to {
+    transform: translateX(0)
+  }
 }
 
 .main {

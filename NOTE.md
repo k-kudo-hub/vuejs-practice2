78f661b03b18e9c643e9fec72f548bd80f3445cc
k-kudo-hub <tongariisaibou@gmail.com>
2020-11-15 11:18:30 +0900

    トランジション効果をCSSトランジションを用いて作成する

---
+++ NOTE.md
@@ -0,0 +1,24 @@
+## Vue.js学習
+
+### 
+
+```vue
+.fade-enter {
+  /* 現れるときの最初の状態 */
+}
+.fade-enter-active {
+  /* 現れるときのトランジションの状態 */
+}
+.fade-enter-to {
+  /* 現れるときの最後の状態 */
+}
+.fade-leave {
+  /* 消えるときの最初の状態 */
+}
+.fade-leave-active {
+  /* 消えるときのトランジションの状態 */
+}
+.fade-leave-to {
+  /* 消えるときの最後の状態 */
+}
+```
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <button @click="show =! show">切り替え</button>
+    <transition name="fade">
+      <p v-if="show">hello</p>
+    </transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      show: true
+    };
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.fade-enter {
+  /* 現れるときの最初の状態 */
+  opacity: 0;
+}
+.fade-enter-active {
+  /* 現れるときのトランジションの状態 */
+  transition: opacity .5s;
+}
+.fade-enter-to {
+  /* 現れるときの最後の状態 */
+  opacity: 0;
+}
+.fade-leave {
+  /* 消えるときの最初の状態 */
+  opacity: 1;
+}
+.fade-leave-active {
+  /* 消えるときのトランジションの状態 */
+  transition: opacity .5s;
+}
+.fade-leave-to {
+  /* 消えるときの最後の状態 */
+  opacity: 0;
+}
+
+.main {
+  width: 70%;
+  margin: auto;
+  padding-top: 5rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
